433f63bcc7380819e2dfb4f43af9f25890b7d674
Eyihbanner1 <magpiep18@gmail.com>
2022-06-29 21:36:39 +0100

    add comment

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const adviceText = document.querySelector('#advice-text');
 const randomBtn = document.querySelector('#random-btn');
 
 async function getAdvice(){
+    // fetch advice from api
     const response = await fetch(apiUrl);
+
+    // convert response to json
     const data = await response.json();
 
+    // use data to update UI or manipulate DOM
     adviceId.innerHTML = `Advice #${data.slip.id}`;
     adviceText.innerHTML = data.slip.advice;
 }
 
+
+// add event listener to random button
 randomBtn.addEventListener('click', getAdvice);
 
+// call getAdvice function on load
 getAdvice();
